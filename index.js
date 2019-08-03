4f6e3a4de09522e8ad6ab32f574209554b60c7d1
mono motapa <bahobab@hotmail.com>
2019-08-03 12:19:52 -0500

    Fixed assets path in relation with express static directory

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(["/", "src"]));
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, "index.html"));
     })
 }
